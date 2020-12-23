27e22925442898fa04305575402af26878c5fd55
berserks03 <berserks@inbox.lv>
2020-12-23 17:05:03 +0200

    added login input fields

---
+++ src/components/userLogin/userLogin.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from 'react';
+import './userLogin.scss';
+
+export const UserLogin: FC = () => {
+  return (
+    <div>
+      <h1>Login Form</h1>
+      <div>User Name</div>
+      <input type="text" name="" id="" />
+      <div>Password</div>
+      <input type="password" name="" id="" />
+    </div>
+  );
+};
