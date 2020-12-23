dc225ce8bbff48f069b8acfa9990c5fc69cf4c0b
berserks03 <berserks@inbox.lv>
2020-12-23 17:20:39 +0200

    added color

--- src/components/userLogin/userLogin.tsx
+++ src/components/userLogin/userLogin.tsx
@@ -3,12 +3,13 @@ import './userLogin.scss';
 
 export const UserLogin: FC = () => {
   return (
-    <div>
+    <div className='UserLogin'>
       <h1>Login Form</h1>
       <div>User Name</div>
       <input type="text" name="" id="" />
       <div>Password</div>
       <input type="password" name="" id="" />
+      <button type="submit">Login</button>
     </div>
   );
 };
