8ebb30473637319d7cc41ed12ba04d68818ebe23
Ainika <aina.kostina@gmail.com>
2020-12-26 21:03:50 +0200

    Added back button, little cards to articles page

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,22 +1,24 @@
 import React, { FC } from 'react';
+import { useHistory } from 'react-router-dom';
 import { Nav } from '../nav/nav';
 import logo from '../../assets/blog_logo.png';
 import './header.scss';
 
-
-
 export const Header: FC = () => {
+  const history = useHistory();
+
+  const logoClickHandler = () => {
+    history.push('/home');
+  };
 
   return (
     <header className="header">
       <div className="container">
         <div className="row middle-xs">
           <div className="col-xs-2">
-            <img
-              src={logo}
-              alt=""
-              width="100"
-            />
+            <div className="logo" role="button" onClick={logoClickHandler} tabIndex={0} onKeyDown={logoClickHandler}>
+              <img src={logo} alt="" width="100" />
+            </div>
           </div>
           <div className="col-xs-8">
             <Nav />
